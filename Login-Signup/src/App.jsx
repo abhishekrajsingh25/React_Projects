a65e3da649ced9c3795704fef8829fe68a0a93dd
Abhishek Raj Singh <abhishekrajsingh2509@gmail.com>
2024-10-24 18:54:33 +0530

    Login Signup

---
+++ Login-Signup/src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./App.css";
+import LoginSignup from "./components/login-signup/LoginSignup";
+
+const App = () => {
+  return (
+    <div>
+      <LoginSignup />
+    </div>
+  );
+};
+
+export default App;
